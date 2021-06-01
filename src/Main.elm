076866ac389cadb4f4db9a9a321236cd63c03254
Alexander Kotov <alexanderko@interlink-ua.com>
2021-06-02 00:46:14 +0300

    Implement action ticker

---
+++ src/Main.elm
@@ -0,0 +1,121 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (action)
+import Process
+import Task exposing (perform)
+import Tuple exposing (first)
+import Tuple exposing (second)
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type Rotation
+    = Clockwise
+    | CounterCw
+
+
+type Action
+    = TornClockwise
+    | TornCounterCw
+    | Step
+    | Hit
+
+
+type alias Script =
+    List Action
+
+
+type alias Model =
+    { script : Script
+    , run : ( Script, Script )
+    }
+
+
+stepAndHit : Script
+stepAndHit =
+    [ Step, Hit ]
+
+
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( Model stepAndHit ( [], stepAndHit ), scheduleTick )
+
+
+type Msg
+    = StartGame
+    | Tick
+
+
+scheduleTick : Cmd Msg
+scheduleTick =
+    Process.sleep (2 * 1000)
+        |> perform (always Tick)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        StartGame ->
+            ( { model | run = ( [], model.script ) }, scheduleTick )
+
+        Tick ->
+            case model.run of
+                ( _, [] ) ->
+                    ( model, Cmd.none )
+
+                ( done, current :: next ) ->
+                    let
+                        run =
+                            ( current :: done, next )
+                    in
+                    ( { model | run = run }, scheduleTick )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ actionsView (model.run |> first)
+        , hr [] []
+        , actionsView (model.run |> second)
+        ]
+
+
+actionsView : Script -> Html Msg
+actionsView actions =
+    ul [] (List.map actionView actions)
+
+
+actionView : Action -> Html Msg
+actionView action =
+    li [] [ action |> toString |> text ]
+
+
+toString : Action -> String
+toString action =
+    case action of
+        TornClockwise ->
+            "Clockwise"
+
+        TornCounterCw ->
+            "CounterCw"
+
+        Step ->
+            "Step"
+
+        Hit ->
+            "Hit"
